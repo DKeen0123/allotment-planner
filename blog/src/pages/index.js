db11294b228a35baa3f1c1820a9066c6cfb3b85e
Daniel Keen <keendaniel01@gmail.com>
2019-09-11 17:26:15 +0100

    Prep for production: Add manifest

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import ComingSoon from '../components/ComingSoon';
 
-export default () => <ComingSoon />;
+export default () => (
+	<React.Fragment>
+		<Helmet>
+			<meta charSet="utf-8" />
+			<title>Allotment Planner</title>
+		</Helmet>
+		<ComingSoon />
+	</React.Fragment>
+);
